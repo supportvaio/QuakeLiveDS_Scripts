3de9714a35d16cd0473a67c4781a71f9b848764f
dark-saber <darksab0r@gmail.com>
2015-11-02 16:43:49 +0500

    Fixed wrong server.sh spelling

--- scripts-standalone/quakestart.sh
+++ scripts-standalone/quakestart.sh
@@ -6,7 +6,7 @@
 
 # Defining variables.
 export location="EKB, RU"
-export qPathToStartScript="~/steamcmd/steamapps/common/qlds/run_server_x32.sh"
+export qPathToStartScript="~/steamcmd/steamapps/common/qlds/run_server_x64.sh"
 sponsortag="$location"
 
 gameport=`expr $1 + 27960`
