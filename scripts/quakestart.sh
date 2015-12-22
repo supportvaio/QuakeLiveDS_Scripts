c2d9fdd5bc15af45789542a925507540fc5e02d9
Dmitry Kharitonov <darksab0r@gmail.com>
2015-12-23 01:22:59 +0500

    Trying to push #0 CA on top of other servers

--- scripts/quakestart.sh
+++ scripts/quakestart.sh
@@ -31,7 +31,7 @@ echo "Starting clan arena server 1..."
 exec $qPathToMinqlxStartScript \
     +set net_strict 1 \
     +set net_port $gameport \
-    +set sv_hostname "    #$servernum let's start cooking Clan Arena | Elo shuffle" \
+    +set sv_hostname "     #$servernum let's start cooking Clan Arena | Elo shuffle" \
     +set zmq_rcon_enable 1 \
     +set zmq_rcon_password "$qRconPasswordPurgery" \
     +set zmq_rcon_port $rconport \
