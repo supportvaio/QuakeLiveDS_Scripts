67d103d972e8dcaaac3bfff9574390d227cd21f7
Dmitry Kharitonov <darksab0r@gmail.com>
2016-06-04 12:38:18 +0500

    Fixed local paths in extraplugins-update

--- autoconfig-server/extraplugins-update.sh
+++ autoconfig-server/extraplugins-update.sh
@@ -3,35 +3,35 @@
 
 cd /home/qlserver
 echo "Updating tjone270-minqlx-plugins..."
-cd /home/qlserver/tjone270-minqlx-plugins
+cd /home/qlserver/tjone270-minqlx-plugins/Quake-Live
 git pull
 cd /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins/
 ln -s /home/qlserver/tjone270-minqlx-plugins/Quake-Live/minqlx-plugins/* .
 
 cd /home/qlserver
 echo "Updating cstewart90-minqlx-plugins..."
-cd /home/qlserver/cstewart90-minqlx-plugins
+cd /home/qlserver/cstewart90-minqlx-plugins/minqlx-plugins
 git pull
 cd /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins/
 ln -s /home/qlserver/cstewart90-minqlx-plugins/minqlx-plugins/* .
 
 cd /home/qlserver
 echo "Updating dsverdlo-minqlx-plugins..."
-cd /home/qlserver/dsverdlo-minqlx-plugins
+cd /home/qlserver/dsverdlo-minqlx-plugins/minqlx-plugins
 git pull
 cd /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins/
 ln -s /home/qlserver/dsverdlo-minqlx-plugins/minqlx-plugins/* .
 
 cd /home/qlserver
 echo "Updating x0rnn-minqlx-plugins..."
-cd /home/qlserver/x0rnn-minqlx-plugins
+cd /home/qlserver/x0rnn-minqlx-plugins/minqlx-plugins
 git pull
 cd /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins/
 ln -s /home/qlserver/x0rnn-minqlx-plugins/minqlx-plugins/* .
 
 cd /home/qlserver
 echo "Updating x0rnn-minqlx-plugins-1 (Race)..."
-cd /home/qlserver/x0rnn-minqlx-plugins-1
+cd /home/qlserver/x0rnn-minqlx-plugins-1/minqlx-plugins-1
 git pull
 cd /home/qlserver/steamcmd/steamapps/common/qlds/minqlx-plugins/
 ln -s /home/qlserver/x0rnn-minqlx-plugins-1/minqlx-plugins-1/* .
