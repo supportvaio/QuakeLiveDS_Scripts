c5caae1fc7269e92cd5a61af856adea502c653de
dark-saber <darksab0r@gmail.com>
2016-07-30 20:21:19 +0600

    Adding git to installable packages

--- autoconfig-server/quakeinstall-root.sh
+++ autoconfig-server/quakeinstall-root.sh
@@ -12,7 +12,7 @@ echo "Updating 'apt-get'..."
 apt-get update
 clear
 echo "Installing packages..."
-apt-get -y install apache2 python3 python-setuptools lib32gcc1 curl nano samba build-essential python-dev unzip dos2unix mailutils wget lib32z1 lib32stdc++6 libc6 supervisor
+apt-get -y install apache2 python3 python-setuptools lib32gcc1 curl git nano samba build-essential python-dev unzip dos2unix mailutils wget lib32z1 lib32stdc++6 libc6 supervisor
 clear
 echo "Installing ZeroMQ library..."
 # we use '--without-libsodium' because I encounter many problems with trying to configure with it.
